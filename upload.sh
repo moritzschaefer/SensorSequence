3770008f2601a0bf6505f37e363dfc471298df4e
Moritz Schäfer <mail@moritzs.de>
2015-01-14 15:06:27 +0100

    fix upload.sh

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ if [ "$?" -ne "0" ]; then
    echo "COMPILE ERROR!"
    exit 1
 fi
-if "$#" -eq "3" ]; then
+if [ "$#" -eq "3" ]; then
   node0=$1
   node1=$2
   node2=$3
@@ -17,7 +17,7 @@ else
   node1=1
 fi
 echo "Programming Node 1"
-if [ -n $node2 ]; then
+if [ -n "$node2" ]; then
   make tmote reinstall,2 bsl,/dev/ttyUSB$node2
 fi
 make tmote reinstall,1 bsl,/dev/ttyUSB$node1 || {
