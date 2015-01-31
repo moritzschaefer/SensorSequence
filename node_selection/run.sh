25281c4b16bd94c79f33b2fcbf29fcba877fcb33
senexger <senexger@gmail.com>
2015-01-31 08:12:16 +0100

    run.sh benötigt nur datei als input, Pfad und Nodes sind nicht nötig

--- node_selection/run.sh
+++ node_selection/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
-cp measurement.txt ../node_algorithm/seq16ch_1.txt
+PFAD=$(cd $(dirname $0) 2>/dev/null && pwd)
+echo $PFAD
+#echo "Inputfile: $1"
+cp $1 ../node_algorithm/seq16ch_1.txt
 cd ../node_algorithm
+shift
+sed -i -e 43c"Project = \"$PFAD\", " seq16ch_1.txt 
 R CMD BATCH probabilisticSeq-v2.5.R
 echo "Computed result is:"
 cat finalresult.txt
-
