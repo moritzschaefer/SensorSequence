9ac50bcdcfe92cd5a79c927d04240a69589ea841
Moritz Schäfer <mail@moritzs.de>
2015-02-01 23:07:16 +0100

    use all 16 channels

--- node_selection/constants.h
+++ node_selection/constants.h
@@ -8,12 +8,10 @@
 #define NUM_CHANNELS 16
 #define NUM_MEASUREMENTS_PER_NODE 48
 
-#define EMPTY_PACKETS 15
-
 #define SEND_SINGLE_MEASUREMENT_DATA 1
 
-uint8_t numChannels = 10;
-uint8_t channels[] = {11,12,13,14,15,16,17,18,19,20}; // TODO: right now first channel must be 11
+uint8_t numChannels = 16;
+uint8_t channels[] = {11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26}; // TODO: right now first channel must be 11
 
 #endif
 
