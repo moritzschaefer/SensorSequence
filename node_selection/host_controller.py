f5cac0264cb65aace6e6b6b9dac18249f7059081
Moritz Schäfer <mail@moritzs.de>
2015-02-01 23:46:58 +0100

    untested fixing of host_controller.py

--- node_selection/host_controller.py
+++ node_selection/host_controller.py
@@ -15,8 +15,10 @@ from tinyos.packet.Serial import Serial
 
 
 class HostController:
-    def __init__(self, motestring):
-        self.marked = False
+    def __init__(self, motestring, outfile):
+        if outfile:
+            self.outfile = open(outfile, 'w')
+
         self.mif = MoteIF.MoteIF()
         self.tos_source = self.mif.addSource(motestring)
         self.mif.addListener(self, MeasurementData.MeasurementData)
@@ -28,67 +30,59 @@ class HostController:
         if msg.get_amType()==137:
             if m.get_rss() != 0:
                 output_line = '\t'.join((str(int(x)) for x in (m.get_senderNodeId(), m.get_receiverNodeId(), m.get_channel(), m.get_rss(), 31, time.time(), m.get_measurementNum()))) + '\n'
-                if args.out_file:
+                try:
+                    self.outfile.write(output_line)
+                except NameError:
                     print output_line
-                else:
-                    fobj_in = open("args.out_file")
-                    fobj_out = open("args.out_file","w")
-                    print output_line
-                    fobj_in.close()
-                    fobj_out.close()
-            sys.stdout.flush()
+                    sys.stdout.flush()
         elif msg.get_amType()==142:
+            try:
+                self.outfile.close()
+            except NameError:
+                pass
             sys.exit(0)
 
 
-    def send(self):
+    def send(self, args):
 
         smsg = SerialControl.SerialControl()
         smsg.set_cmd(0)
         # 0 means, don't change the value
-        smsg.set_num_measurements(20)
-        smsg.set_channel_wait_time(self.channel_wait_time)
-        smsg.set_sender_channel_wait_time(self.sender_channel_wait_time)
-        smsg.set_id_request_wait_time(0)
-        smsg.set_data_collection_channel(0)
+        smsg.set_num_measurements(args.measurements)
+        smsg.set_channel_wait_time(args.channelWait)
+        smsg.set_sender_channel_wait_time(args.senderChannelWait)
+        smsg.set_id_request_wait_time(args.idWait)
+        smsg.set_data_collection_channel(0) # not used
         self.mif.sendMsg(self.tos_source, 0xFFFF, smsg.get_amType(), 0, smsg)
-
-    def main_loop(self):
-        while 1:
-            line = raw_input()
-            try:
-                self.sender_channel_wait_time, self.channel_wait_time = (int(x) for x in line.split())
-            except ValueError:
-                self.sender_channel_wait_time, self.channel_wait_time = (110,60)
-
-            self.send()
 
 def main():
 
     #argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--measurements", default=20, type=int)
-    parser.add_argument("--channelWait", default=0, type=int)
-    parser.add_argument("--SenderchannelWait", default=0, type=int)
-    parser.add_argument("--idWait", default=0, type=int)
-    parser.add_argument("--collectionChannel", default=0, type=int)
-    parser.add_argument("--out_file", type=str)
-    parser.add_argument("--nodepath", default="serial@/dev/ttyUSB0:115200", type=str)
+    # 0 is using values from nesc code
+    parser.add_argument('--measurements', default=20, type=int, helper='How many measurements per node and channel')
+    parser.add_argument('--channelWait', default=0, type=int, helper='How much time to wait after a channel switch')
+    parser.add_argument('--senderChannelWait', default=0, type=int, helper='How much time to wait after a channel switch (sink node)')
+    parser.add_argument('--idWait', default=0, type=int, helper='How much time to wait for node ids?')
+    #parser.add_argument('--collectionChannel', default=0, type=int, helper='') # not supported yet
+    parser.add_argument('--outfile', type=str, helper='Write to stdout or to a filename')
+    parser.add_argument('--nodepath', default='serial@/dev/ttyUSB0:115200', type=str)
     args = parser.parse_args()
 
     print args.nodepath
+    # TODO: print usage if -h in arguments
 
-    if '-h' in sys.argv:
-        print "Usage:", sys.argv[0], arg.nodepath
-        sys.exit()
-    if len(sys.argv) < 2:
-        dl = HostController("serial@/dev/ttyUSB0:115200")
-    else:
-        dl = HostController(sys.argv[1])
+    dl = HostController(args.nodepath, args.outfile)
+    dl.send(args)
 
-    dl.main_loop()
+    # do nothing
+    while True:
+        try:
+            raw_input()
+        except KeyboardInterrupt:
+            print "Program was exited by user"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
