0d302f8a5c4fb8b9128ef51fe548f73015955956
Moritz Schäfer <mail@moritzs.de>
2015-02-02 01:29:20 +0100

    change datatype

--- node_selection/MeasurementData.h
+++ node_selection/MeasurementData.h
@@ -2,7 +2,7 @@
 #define MEASUREMENT_DATA_H
 
 typedef nx_struct measurement_data {
-  nx_int16_t rss;
+  nx_int8_t rss;
   nx_uint16_t senderNodeId;
   nx_uint16_t receiverNodeId;
   nx_uint8_t channel;
